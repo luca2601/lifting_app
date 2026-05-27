78aa3d8c140141ee2c93405a44db866a582d07f1
Luca Leonhart <luca.leonhart@googlemail.com>
2026-05-27 14:20:23 +0200

    Update service worker cache version and use network-first fetch

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lift-log-v1";
+const CACHE_NAME = "lift-log-v2";
 const APP_FILES = [
   "./",
   "./index.html",
@@ -9,7 +9,9 @@ const APP_FILES = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
+  );
   self.skipWaiting();
 });
 
@@ -24,13 +26,14 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached || fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
-    )
+      .catch(() => caches.match(event.request))
   );
 });
